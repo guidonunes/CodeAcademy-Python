c4d63dd966ad285cd1b8f11b77735b341ab6175c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-23 10:29:45 -0300

    add print statement

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -5,3 +5,5 @@ num_of_children = 3
 smoker = 0
 
 insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
