2251799458901c38d70b913192902535c73b1332
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-24 10:02:54 -0300

    implement logic for male v. female factor

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -28,3 +28,8 @@ print("The change in estimate insurance cost after increasing BMI by 3.1 is " + 
 # Male vs. Female Factor
 bmi = 26.2
 sex = 1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated cost for being male instead of female is " + str(change_in_insurance_cost) + " dollars.")
