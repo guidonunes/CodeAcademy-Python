ee710f4ea26446e10c46cff25adc0ea3fc7beb0d
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-24 08:41:28 -0300

    increment age variable by 4

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -7,3 +7,5 @@ smoker = 0
 insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 
 print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
+
+age += 4
