242a7b8d210ea56d7c36216c168794494060eddd
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-24 08:53:09 -0300

    add print statement

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -11,3 +11,5 @@ print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
 age += 4
 new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in cost of insurance after increasing the age by 4 years is " str(change_in_insurance_cost) + " dollars.")
