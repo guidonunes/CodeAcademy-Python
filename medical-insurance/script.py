870fd46201f59b4a418310b85167f047992c0e8a
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-25 11:41:39 -0300

    add initial variables for Omar

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -1,8 +1,18 @@
+
+# Initial variables for Maria
 age = 28
 sex = 0
 bmi = 26.2
 num_of_children = 3
 smoker = 0
+
+# Initial variables for Omar
+age = 32
+sex = 1
+bmi = 28.2
+num_of_children = 1
+smoker = 1
+
 
 # Add insurance estimate formula below
 insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
