f6b5d6de41321af78f43b6f89b3d24b3fef9bc6e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-25 11:55:42 -0300

    refactor code to display costs for Omar

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -13,3 +13,15 @@ smoker = 0
 insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
 
 print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
+
+# Initial variables for Omar
+age = 35
+sex = 1
+bmi = 22.2
+num_of_children = 0
+smoker = 1
+
+# Estimate Omar's insurance cost
+insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+
+print("The estimated insurance cost for Omar is " + str(insurance_cost) + " dollars.")
