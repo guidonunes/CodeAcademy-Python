b41f911cdbe8791f4f9882bd7e05549c729c067e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-25 11:54:33 -0300

    refactor code to display costs for Maria

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -9,57 +9,7 @@ bmi = 26.2
 num_of_children = 3
 smoker = 0
 
-# Initial variables for Omar
-age = 32
-sex = 1
-bmi = 28.2
-num_of_children = 1
-smoker = 1
+# Estimate Maria's insurance cost
+insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
 
-
-# Add insurance estimate formula below
-insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
-
-# Age Factor
-age += 4
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print("The change in cost of insurance after increasing the age by 4 years is " + str(change_in_insurance_cost) + " dollars.")
-
-# BMI Factor
-age = 28
-bmi += 3.1
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print("The change in estimate insurance cost after increasing BMI by 3.1 is " + str(change_in_insurance_cost) + " dollars.")
-
-# Male vs. Female Factor
-bmi = 26.2
-sex = 1
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print("The change in estimated cost for being male instead of female is " + str(change_in_insurance_cost) + " dollars.")
-
-# Smoker Factor
-smoker = 1
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print("-> The change in estimated cost for being a smoker is " + str(change_in_insurance_cost) + " dollars.")
-
-# Children Factor
-smoker = 0
-num_of_children = 0
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print("-> The change in estimated cost for being childless is " + str(change_in_insurance_cost) + " dollars.")
+print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
