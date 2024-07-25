f62d281f7564dd39d513093a3d30b94a1c137910
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-25 13:44:38 -0300

    refactor Maria insurance cost

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -11,7 +11,7 @@ num_of_children = 3
 smoker = 0
 
 # Estimate Maria's insurance cost
-insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+maria_insurance_cost = calculate_insurance_cost()
 
 print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
 
