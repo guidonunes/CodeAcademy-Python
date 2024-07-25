d8647e1aad614f75f6be362a9fd15b3e93ac3930
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-25 14:10:24 -0300

    fix str method and deleted duplicate print statement

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -1,16 +1,15 @@
 def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker):
     estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-    print("The estimated insurance cost for this person is " + str(estimated_cost) + " dollars.")
     return estimated_cost
 
 
 # Estimate Maria's insurance cost
 maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)
 
-print("The estimated insurance cost for Maria is " + str(calculate_insurance_cost) + " dollars.")
+print("The estimated insurance cost for Maria is " + str(maria_insurance_cost) + " dollars.")
 
 
 # Estimate Omar's insurance cost
 omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1)
 
-print("The estimated insurance cost for Omar is " + str(calculate_insurance_cost) + " dollars.")
+print("The estimated insurance cost for Omar is " + str(omar_insurance_cost) + " dollars.")
