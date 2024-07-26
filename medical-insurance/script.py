86d715eed53788f46f66b1e18558baa345b1a78a
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-26 09:54:50 -0300

    pass name as parameter in print statement

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -1,6 +1,6 @@
 def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
     estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-    print("The estimated insurance cost for this person is " + str(estimated_cost) + "dollars.")
+    print("The estimated insurance cost for " + name + " is " + str(estimated_cost) + "dollars.")
     return estimated_cost
 
 
