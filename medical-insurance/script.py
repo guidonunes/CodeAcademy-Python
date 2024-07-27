9938884b241d988fb6b863087ac57ea32bc07398
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-27 18:41:36 -0300

    add missing argument

--- medical-insurance/script.py
+++ medical-insurance/script.py
@@ -13,4 +13,4 @@ def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
 
 
 # Estimate Maria's insurance cost
-keanu_insurance_cost = calculate_insurance_cost(name = 'Keanu', age = 29, sex = 1, num_of_children = 3, smoker = 1)
+keanu_insurance_cost = calculate_insurance_cost(name = 'Keanu', age = 29, sex = 1, bmi=26.2, num_of_children = 3, smoker = 1)
