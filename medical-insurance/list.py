074a6cf5c21ca52c8864aeb73adf564ae144465f
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-30 12:22:18 -0300

    append data to names and insurance costs

--- medical-insurance/list.py
+++ medical-insurance/list.py
@@ -1,2 +1,8 @@
 names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
 insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
+
+
+names.append("Priscilla")
+print(names)
+insurance_costs.append(8320.0)
+print(insurance_costs)
