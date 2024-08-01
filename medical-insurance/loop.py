7efa36209b3b4cd92672d929885ef3ab7d38b488
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-01 15:49:38 -0300

    implement for loop to get the sum of insurance costs

--- medical-insurance/loop.py
+++ medical-insurance/loop.py
@@ -3,3 +3,8 @@ estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 700
 actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
 
 total_cost = 0
+
+for cost in actual_insurance_costs:
+  total_cost += cost
+
+print(total_cost)
