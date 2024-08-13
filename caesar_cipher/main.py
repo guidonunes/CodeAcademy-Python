ef8cf97a675a28e4b7ec30109492699bc68cbef9
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-13 14:22:51 -0300

    add variable with encoded string

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -1,1 +1,2 @@
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+encoded = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
