0099f8f223911dd1173dbf82b925e5409cd360fb
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-13 14:31:46 -0300

    refactor code into a function

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -1,12 +1,15 @@
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 encoded = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
+def decode_message(message, offset):
+    decoded = ""
+    for char in message:
+        if char in ALPHABET:
+            index = (ALPHABET.index(char) + offset) % len(ALPHABET)
+            decoded += ALPHABET[index]
+        else:
+            decoded += char
+    return decoded
 
-decoded = ""
-for char in encoded:
-  if char in ALPHABET:
-    index = (ALPHABET.index(char) + 10) % len(ALPHABET)
-    decoded += ALPHABET[index]
-  else:
-    decoded += char
-print(decoded)
+decoded_message = decode_message(encoded, 10)
+print(decoded_message)
