a0edcccc8df6d10a7cb7b2b77f64ff3451ad47ce
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-13 14:56:40 -0300

    add encode function logic

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -11,5 +11,17 @@ def decode_message(message, offset):
             decoded += char
     return decoded
 
+# fix the bug in the encode_message function
+def encode_message(message, offset):
+    encoded = ""
+    for char in message:
+        if char in ALPHABET:
+            index = (ALPHABET.index(char) - offset+1) % len(ALPHABET)
+            print(index)  # Correct: Add offset for encoding
+            encoded += ALPHABET[index]
+        else:
+            encoded += char  # Leave non-alphabet characters unchanged
+    return encoded
+
 decoded_message = decode_message(encoded, 10)
 print(decoded_message)
